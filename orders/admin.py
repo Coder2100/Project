a0778f764c5543bc9a3f7a66052773ce581791d3
@Coder2100 <mtotsoz@gmail.com>
2019-05-10 07:21:27 +0200

    Frontend design

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+#inform admin about my models
+#from .models import Order
+
 # Register your models here.
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+# import Order model from model
+#from .models import Order
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+   # return HttpResponse("Project 3: TODO")
+   return render(request, "orders/index.html")
