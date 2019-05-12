9064237d81cfcb44ee0e1f9801c904ac68e1e138
@Coder2100 <mtotsoz@gmail.com>
2019-05-12 23:52:53 +0200

    menu model

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from django.urls import reverse
 # import Order model from model
-from .models import Pasta, MenuCatalogue
+from .models import Pasta, MenuCatalogue, Topping, RegularPizza, SicilianPizza, SicilianPizza, Sub,Salad # DinnerPlatter
 
 # Create your views here.
 def index(request):
@@ -24,7 +25,14 @@ def menu(request):
 
    context = {
       "pastas": Pasta.objects.all(),
-      "MenuCatalogues": MenuCatalogue.objects.all()
+      "menuCatalogues": MenuCatalogue.objects.all(),
+      "toppings": Topping.objects.all(),
+      "regularyPizzas": RegularPizza.objects.all(),
+      "sicilianPizzas": SicilianPizza.objects.all(),
+      "subs": Sub.objects.all(),
+     # "dinnerPlatters": DinnerPlatter.objects.all(),
+      "salads": Salad.objects.all()
+
    }
    return render(request, "orders/menu.html", context)
 
